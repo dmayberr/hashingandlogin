66d8c3fb45838430a5ff5a88112b761562d3d611
Darrell L. Mayberry <dmayberr@gmail.com>
2021-01-03 19:47:04 -0500

    Connected registration form to app.py to render.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from models import User, db, connect_db
-from forms import User
+from forms import NewUserForm
 
 app = Flask(__name__)
 # TODO- Add db name to link below
+app.config['SECRET_KEY'] = "whatever"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///*****'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
@@ -12,15 +13,24 @@ db.init_app(app)
 connect_db(app)
 
 @app.route('/')
-def root():
-    
-    redirect (f"/register")
+def root():    
+    return redirect (f"/register")
     
 @app.route('/register')
 def show_registration_form():
     """Route to get and show registration form to create new users."""
     
-    return render_template('new_user_form.html')
+    form = NewUserForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        
+        flash(f"User created.")
+    return render_template('new_user_form.html', form=form)
 
 @app.route('/register', methods=['POST'])
 def submit_new_user():
@@ -35,7 +45,7 @@ def login():
     return render_template('login_form.html')
 
 @app.route('/login', methods=['POST'])
-def login():
+def handle_login():
     """Route to handle submission of login information"""
     
     return redirect (f"/secret")
