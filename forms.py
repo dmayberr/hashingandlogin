200f0bbc17e722f195ab545ceee4bea347634b8d
Darrell L. Mayberry <dmayberr@gmail.com>
2021-01-03 19:10:58 -0500

    Created new user form and login form in forms.py.

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask.app import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired, Email
+
+class NewUserForm(FlaskForm):
+    """Form for registering and creating a new user."""
+    
+    username = StringField("Username")
+    password = StringField("Password")
+    email = StringField("Email address")
+    first_name = StringField("First name")
+    last_name = StringField("Last name")
+    
+class LoginForm(FlaskForm):
+    """Form for handling user login information."""
+    
+    username = StringField("Username",
+                           validators=[InputRequired()])
+    password = StringField("Password",
+                           validators=[Email()])
